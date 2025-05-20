36dbc93b6fc9cc75d873a96b02ce7c8446c2bcb1
charan <sravanamcharan20@gmail.com>
2025-05-20 17:18:57 +0530

    small chanegs done against history naviagtion

--- gemini-support-buddy/src/App.tsx
+++ gemini-support-buddy/src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Chat from "./pages/History";
+import History from "./pages/History";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/history" element={<Chat />} />
+          <Route path="/history" element={<History />} />
           {/* Catch-all route should be last */}
           <Route path="*" element={<NotFound />} />
         </Routes>
